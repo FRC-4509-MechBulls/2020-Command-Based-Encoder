82974fa007507a5d79781b85f207eb44bcf09717
Andrew Goeden <andrewgoeden@gmail.com>
2020-02-28 23:54:55 -0500

    Added to autonomous command
    Made the autonomous first tilt the cannon and then shoot for 8 seconds (line up robot to shoot into target) then drive forwards for a few seconds.

--- src/main/java/frc/robot/commands/driving/AutoCommand.java
+++ src/main/java/frc/robot/commands/driving/AutoCommand.java
@@ -9,14 +9,19 @@ package frc.robot.commands.driving;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.CannonTiltSubsystem;
 import frc.robot.subsystems.DrivingSubsystem;
+import frc.robot.subsystems.IntakeAndShootSubsystem;
 
 public class AutoCommand extends CommandBase {
   /**
    * Creates a new AutoCommand.
    */
   DrivingSubsystem drivingSubsystem;
+  CannonTiltSubsystem cannonTiltSubsystem;
+  IntakeAndShootSubsystem intakeAndShootSubsystem;
   Timer timer;
+
   public AutoCommand(DrivingSubsystem subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     drivingSubsystem = subsystem;
@@ -27,14 +32,20 @@ public class AutoCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-      timer.reset();
-      timer.start();
+    timer.reset();
+    timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivingSubsystem.drive.arcadeDrive(.5,0);// drive straight at half 
+    cannonTiltSubsystem.shootMode();
+    
+    while (timer.get() < 5) {
+      intakeAndShootSubsystem.enable(0.0, -.95);
+      intakeAndShootSubsystem.index(-1.0);
+    }
+    DrivingSubsystem.drive.arcadeDrive(.5, 0);// drive straight at half
   }
 
   // Called once the command ends or is interrupted.
@@ -45,6 +56,6 @@ public class AutoCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return timer.get() > 3;  // end the command if we have run for at least 3 seconds
+    return timer.get() > 10; // end the command if we have run for at least 3 seconds
   }
 }
