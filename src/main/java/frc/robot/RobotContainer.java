3b36961dcb6b971c47b3965f3e5484e7bad6ba4c
Natasha Setiadi <natset456@gmail.com>
2020-03-04 14:43:01 -0500

    implemented joysticks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.compressor.StartCompressorCommand;
@@ -35,8 +36,10 @@ public class RobotContainer {
 
     //public XboxController1 controller1;
     XboxController controller2 = new XboxController(Constants.XBOX_CONTROLLER_2_PORT);
-    XboxController controller1 = new XboxController(Constants.XBOX_CONTROLLER_1_PORT);
-   
+    // XboxController controller1 = new XboxController(Constants.XBOX_CONTROLLER_1_PORT);
+    Joystick joystick1 = new Joystick(0);
+    Joystick joystick2 = new Joystick(1);
+
     DrivingSubsystem drivingSubsystem = new DrivingSubsystem();
     ClimberSubsystem climberSubsystem = new ClimberSubsystem();
     PnuematicSubsystem pnuematicSubsystem = new PnuematicSubsystem();
@@ -55,11 +58,11 @@ public class RobotContainer {
     }
 
     public double getRightDrive() {
-        return controller1.getY(GenericHID.Hand.kRight);
+        return joystick1.getY();
 		// return Math.abs(n) < 0.1 ? 0 : n;
     }
     public double getLeftDrive() {
-		return controller1.getY(GenericHID.Hand.kLeft);
+		return joystick2.getY();
 		// return Math.abs(n) < 0.1 ? 0 : n;
 	}
 
@@ -68,24 +71,24 @@ public class RobotContainer {
         //womfButton, climberButton, cannonShoot, cannonIntake, climbModeCannon, cannonTiltIntake, cannonTiltShoot
         final JoystickButton cannonTiltIntake = new JoystickButton(controller2, XboxController.Button.kB.value);
         final JoystickButton womfButton = new JoystickButton(controller2, XboxController.Button.kX.value);
-        final JoystickButton climberButton = new JoystickButton(controller1, XboxController.Button.kBumperRight.value);
+      //  final JoystickButton climberButton = new JoystickButton(controller1, XboxController.Button.kBumperRight.value);
         final JoystickButton cannonShoot = new JoystickButton(controller2,  XboxController.Button.kBumperRight.value);
-        final JoystickButton climbModeCannon = new JoystickButton(controller1,  XboxController.Button.kBumperLeft.value);
+       // final JoystickButton climbModeCannon = new JoystickButton(controller1,  XboxController.Button.kBumperLeft.value);
         final JoystickButton cannonTiltShoot = new JoystickButton(controller2,  XboxController.Button.kA.value);
         final JoystickButton cannonIntake= new JoystickButton(controller2,  XboxController.Button.kBumperLeft.value);
         final JoystickButton compressorButton = new JoystickButton(controller2,  XboxController.Button.kY.value);
 
 
-        final JoystickButton indexerShoot= new JoystickButton(controller1,  XboxController.Button.kB.value);
-        final JoystickButton indexerIntake= new JoystickButton(controller1,  XboxController.Button.kA.value);
+      //  final JoystickButton indexerShoot= new JoystickButton(controller1,  XboxController.Button.kB.value);
+       // final JoystickButton indexerIntake= new JoystickButton(controller1,  XboxController.Button.kA.value);
 
-        climberButton.whenPressed(new ClimberCommand(climberSubsystem));
-        climberButton.whenReleased(new TurnOffClimberCommand(climberSubsystem));
+        // climberButton.whenPressed(new ClimberCommand(climberSubsystem));
+        // climberButton.whenReleased(new TurnOffClimberCommand(climberSubsystem));
         womfButton.whenPressed(new ActiveColorCommand(womfSubsystem).alongWith(new CannonWomfMode(cannonTiltSubsystem)));
         cannonTiltIntake.whenPressed(new CannonIntakeMode(cannonTiltSubsystem));
         cannonTiltIntake.whenReleased(new StopTiltCommand(cannonTiltSubsystem));
-        climbModeCannon.whenPressed(new CannonClimbMode(cannonTiltSubsystem));
-        climbModeCannon.whenReleased(new StopTiltCommand(cannonTiltSubsystem));
+        // climbModeCannon.whenPressed(new CannonClimbMode(cannonTiltSubsystem));
+        // climbModeCannon.whenReleased(new StopTiltCommand(cannonTiltSubsystem));
         womfButton.whenReleased(new InactiveColorCommand(womfSubsystem).alongWith(new StopTiltCommand(cannonTiltSubsystem)));
         cannonTiltShoot.whenPressed(new CannonShootMode(cannonTiltSubsystem));
         cannonTiltShoot.whenReleased(new StopTiltCommand(cannonTiltSubsystem));
@@ -93,10 +96,12 @@ public class RobotContainer {
         cannonShoot.whenReleased(new IntakeOffCommand(intakeAndShootSubsystem));
         cannonIntake.whenPressed(new IntakeCommand(intakeAndShootSubsystem));
         cannonIntake.whenReleased(new StopTiltCommand(cannonTiltSubsystem).alongWith(new IntakeOffCommand(intakeAndShootSubsystem)));
-        indexerIntake.whenPressed(new IndexIntakeCommand(intakeAndShootSubsystem));
+        // indexerIntake.whenPressed(new IndexIntakeCommand(intakeAndShootSubsystem));
+
        // indexerIntake.whenReleased(new StopIndexCommand(intakeAndShootSubsystem));
-        indexerShoot.whenPressed(new IndexShooterCommand(intakeAndShootSubsystem));
-        indexerShoot.whenReleased(new StopIndexCommand(intakeAndShootSubsystem));
+
+        // indexerShoot.whenPressed(new IndexShooterCommand(intakeAndShootSubsystem));
+        // indexerShoot.whenReleased(new StopIndexCommand(intakeAndShootSubsystem));
         compressorButton.whenPressed(new StartCompressorCommand(pnuematicSubsystem));
         compressorButton.whenReleased(new StopCompressorCommand(pnuematicSubsystem));
 
